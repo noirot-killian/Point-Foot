362d616683ba242680867672a46ef41c7a572854
NOIROT Killian <noirotkillian@gmail.com>
2020-10-15 17:20:25 +0200

    Création page WPF "VoirJoueur" et affichage des joueurs dans un datagrid (partie 1)

---
+++ Point Foot/VoirJoueur.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Point_Foot
+{
+    /// <summary>
+    /// Logique d'interaction pour VoirJoueur.xaml
+    /// </summary>
+    public partial class VoirJoueur : Page
+    {
+        
+        public VoirJoueur()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVoirJoueur_Click(object sender, RoutedEventArgs e)
+        {
+            dgVoirJoueur.ItemsSource = AdoProfil.All();
+            btnVoirJoueur.IsEnabled = false;
+        }
+
+        private void dgVoirJoueur_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Profil p = (Profil)(dgVoirJoueur.CurrentItem);
+        }
+    }
+}
